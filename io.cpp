525701b79afc796a76d972661379edb273d0a957
Ryan <22482214+RainDroplet@users.noreply.github.com>
2022-01-19 00:26:36 -0800

    Basic setup for the io.cpp file.

---
+++ io.cpp
@@ -0,0 +1,21 @@
+/* 
+Objective: 1. Read a sentence from the console.
+2. Break the sentence into workds using the space characer as a delimiter.
+3. Iterate over each word, if the word is a numeric value then print its value
+doubled, otherwise print out the word, with each output on its own line.
+ */
+
+#include <iostream>
+using namespace std;
+
+class io {
+private:
+    // variables
+public:
+    // functions
+    
+};
+
+int main() {
+    return 0;
+}
