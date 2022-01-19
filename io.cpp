544c1338c3a9b1e6e13f00f4e1e93d91bace1cd6
Ryan <22482214+RainDroplet@users.noreply.github.com>
2022-01-19 01:29:14 -0800

    Finished io.cpp

--- io.cpp
+++ io.cpp
@@ -1,21 +1,30 @@
 /* 
 Objective: 1. Read a sentence from the console.
-2. Break the sentence into workds using the space characer as a delimiter.
+2. Break the sentence into words using the space characer as a delimiter.
 3. Iterate over each word, if the word is a numeric value then print its value
 doubled, otherwise print out the word, with each output on its own line.
  */
 
 #include <iostream>
+#include <string>
+#include <sstream>
 using namespace std;
 
-class io {
-private:
-    // variables
-public:
-    // functions
-    
-};
-
 int main() {
-    return 0;
+    float val;
+    string str;
+    string temp;
+    getline(cin, str);
+
+    stringstream streamer(str);
+
+    while(getline(streamer, temp, ' ')){
+        stringstream stream(temp);
+        if (stream >> val)
+            cout << 2*val << endl;
+        else
+            cout << temp << endl;
+    }
+
+    return EXIT_SUCCESS;
 }
