358a5dd0d24b066861ce785a5fe1644b5ec40fe0
jungjai153 <leer1531@gmail.com>
2022-01-18 17:30:15 -0800

    Update linearsearch.cpp
    Added an iteration of the array

--- linearsearch.cpp
+++ linearsearch.cpp
@@ -1,7 +1,9 @@
 /* 
 Objective: Search an un-ordered array of integer for a value, 
 if the value is found return the index of its potiion in the array. If not foudn then return -1
- */
+*/
 #include <iostream>
 using namespace std;
-int 
+int search (int arr[], int cap, int target) {
+    for(int i = 0; i < cap; i++) //Repeat going through array
+}     
