767b8fef4590caf029d974154ef38159d74a65a0
Ryan <22482214+RainDroplet@users.noreply.github.com>
2022-01-18 18:26:09 -0800

    edited some of the for loop

--- linearsearch.cpp
+++ linearsearch.cpp
@@ -2,6 +2,7 @@
 Objective: Search an un-ordered array of integer for a value, 
 if the value is found return the index of its potiion in the array. If not foudn then return -1
 */
+
 #include <iostream>
 using namespace std;
 
