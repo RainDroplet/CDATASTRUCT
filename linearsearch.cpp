9a6a83e4aa0c2ebd953ce0d4c1a838aeb014c607
Ryan <22482214+RainDroplet@users.noreply.github.com>
2022-01-18 17:04:52 -0800

    Update linearsearch.cpp
    Added an objective

--- linearsearch.cpp
+++ linearsearch.cpp
@@ -1,4 +1,7 @@
-// this is just the example code
+/* 
+Objective: Search an un-ordered array of integer for a value, 
+if the value is found return the index of its potiion in the array. If not foudn then return -1
+ */
 #include <iostream>
 using namespace std;
 int 
