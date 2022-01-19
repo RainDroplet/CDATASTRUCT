6b812bec90eeb22507a3210f03a8b083b72f4b63
Ryan <22482214+RainDroplet@users.noreply.github.com>
2022-01-18 19:12:37 -0800

    Finished linesearch.cpp objective

--- linearsearch.cpp
+++ linearsearch.cpp
@@ -1,13 +1,36 @@
 /* 
 Objective: Search an un-ordered array of integer for a value, 
-if the value is found return the index of its potiion in the array. If not foudn then return -1
+if the value is found return the index of its potiion in the array. If not found then return -1
 */
 
 #include <iostream>
 using namespace std;
 
-int search (int arr[], int target) {
-    for(int i = 0; i < sizeof(arr); i++) {
+int search(int arr[], int arrSize, int target) {
+    // function is checking for the target number in the array
+    int i;
+    for(int i = 0; i < arrSize; i++)
+        if (arr[i] == target)
+            return i;
+    return -1;
+}
 
-    } //Repeat going through array
-}     
+int main() {
+
+    // initializing the variables for the function
+    int myArray[] = {1,2,3,4,5};
+    int myArraySize = sizeof(myArray)/sizeof(myArray[0]);
+    int myNum = 6;
+
+    // calling the function with the variables
+    int result = search(myArray, myArraySize, myNum);
+
+    // returning to terminal with corrisponding results
+    if (result == -1)
+        cout << "Returned result was " << result;
+    else
+        cout << "Element is in index position " << result;
+
+    // exit the terminal/program
+    return 0;
+}
